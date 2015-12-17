9c7e13b71846f5e8adb1bb4b746c526c90739135
Adam Sanderson <lazydayed@gmail.com>
2015-12-17 23:56:54 +0000

    Add testing frameworks
    needs more work

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp");
 var gutil = require("gulp-util");
 var webpack = require("webpack");
+
+var babel = require('gulp-babel');
+var tape = require('gulp-tape');
+
 var WebpackDevServer = require("webpack-dev-server");
 var webpackConfig = require("./webpack.config.js");
 
@@ -78,4 +82,10 @@ gulp.task("webpack-dev-server", function(callback) {
 		if(err) throw new gutil.PluginError("webpack-dev-server", err);
 		gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
 	});
+});
+
+gulp.task("test", function() {
+	return gulp.src('src/*.test.js')
+	.pipe(babel())
+    .pipe(tape());	
 });
