f256de9222b3c2fd5a000b5c3ee2e78938fa640b
Adam Sanderson <lazydayed@gmail.com>
2015-12-20 14:28:08 +0000

    Add responsive fixes

--- src/components/MetaData.js
+++ src/components/MetaData.js
@@ -2,18 +2,18 @@
 
 import React from 'react';
 
-let MetaData = React.createClass({	
-  	render() {	  	
+let MetaData = React.createClass({
+  	render() {
 	  	let topic = this.props.topic || {};
-	  		    
+
 	    return (
 	    	<div className="bw-meta-container">
 	        	<h3>Information on topic "{topic.label}"</h3>
-	        	<ul>
-	        		<li>Total mentions: {topic.volume || 0}</li>
-	        		<li>Positive mentions: {topic.sentimentPositive || 0}</li>
-	        		<li>Neutral mentions: {topic.sentimentNeutral || 0}</li>
-	        		<li>Negative mentions: {topic.sentimentNegative || 0}</li>
+	        	<ul className="bw-meta-list">
+	        		<li className="bw-meta-total">Total mentions: {topic.volume || 0}</li>
+	        		<li>Positive mentions: <span className="bw-positive">{topic.sentimentPositive || 0}</span></li>
+	        		<li>Neutral mentions: <span>{topic.sentimentNeutral || 0}</span></li>
+	        		<li>Negative mentions: <span className="bw-negative">{topic.sentimentNegative || 0}</span></li>
 	        	</ul>
 	      	</div>
 	    );
