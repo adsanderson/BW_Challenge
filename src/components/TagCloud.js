95febf6d170bb328d36893e6f64b13fec6c2a5f8
Adam Sanderson <lazydayed@gmail.com>
2015-12-21 08:24:35 +0000

    add additional react component tests

--- src/components/TagCloud.js
+++ src/components/TagCloud.js
@@ -4,42 +4,42 @@ import ReactDOM from 'react-dom';
 import MetaData from './MetaData';
 
 var TagCloud = React.createClass({
-	  componentDidMount() {    	
-   this.props.fetchTopics();
- },
-  	handleListItemClick(index) {  		
-  		  this.props.selectTopic(index);
-  	},
-  	render() {	  	
-	  	    let { topics, selectedTopic } = this.props.topics;	  	
-	  	    let topicsNodes = topics.map((topic, index) => {  	
-	  		  let activeTopic = '';
-	  		  if (index === selectedTopic) {
-	  			  activeTopic = ' bw-tag-active';
-	  		}
-	  		  let listItemProps = {
-	  			  key: topic.id,
-	  			  onClick: this.handleListItemClick.bind(this, index),
-	  			  className: 'bw-tag-topic ' + topic.className + activeTopic
-	  		};	
-	  		  return <div {...listItemProps}>{topic.label}</div>;
-	  	});
-	    
-	  	    let metaDataProps = {
-	  		  topic: topics[selectedTopic]
-	  	};
+  componentDidMount() {
+    this.props.fetchTopics();
+  },
+  handleListItemClick(index) {
+    this.props.selectTopic(index);
+  },
+  render() {
+    let { topics, selectedTopic } = this.props.topics;
+    let topicsNodes = topics.map((topic, index) => {
+      let activeTopic = '';
+      if (index === selectedTopic) {
+        activeTopic = ' bw-tag-active';
+      }
+      let listItemProps = {
+        key: topic.id,
+        onClick: this.handleListItemClick.bind(this, index),
+        className: 'bw-tag-topic ' + topic.className + activeTopic
+      };
+      return <div {...listItemProps}>{topic.label}</div>;
+    });
 
-	  	    let meta = (selectedTopic === -1) ? null : <MetaData {...metaDataProps}/>;
+    let metaDataProps = {
+      topic: topics[selectedTopic]
+    };
 
-	        return (
-	    	<div className="bw-container">
-	    		<div className="bw-tag">
-	        		{topicsNodes}
-	      		</div>
-     			{meta}
-	      	</div>
-	    );
-  	}
+    let meta = (selectedTopic === -1) ? null : <MetaData {...metaDataProps}/>;
+
+    return (
+      <div className="bw-container">
+        <div className="bw-tag">
+          {topicsNodes}
+        </div>
+        {meta}
+      </div>
+    );
+  }
 });
 
 export default TagCloud;
--- test/MetaData.test.js
+++ test/MetaData.test.js
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
 import MetaData from '../src/components/MetaData';
 
-test('mapTopicsJsonToState: expect the the topics to not be in the same order', function (t) {
+test('mapTopicsJsonToState: expect no label when there is no topics in props', function (t) {
 
 	let sd = ReactTestUtils.createRenderer();
 
@@ -18,6 +18,38 @@ test('mapTopicsJsonToState: expect the the topics to not be in the same order', 
 	console.log(shallowMetaData.props.children[0].props.children);
 
 	t.deepEqual(shallowMetaData.props.children[0], (<h3>Information on topic "{undefined}"</h3>));
+
+	let metaList = shallowMetaData.props.children[1].props.children;
+
+	t.equal(metaList[0].props.children.join(''), ('Total mentions: 0'));
+
+  t.end();
+});
+
+test('mapTopicsJsonToState: expect no label when there is no topics in props', function (t) {
+
+	let sd = ReactTestUtils.createRenderer();
+
+	let metaDataProps = {
+		topic: {
+			label: 'Test Label',
+			volume: 6
+		}
+	};
+
+	sd.render(
+	  <MetaData {...metaDataProps}/>
+	);
+
+	let shallowMetaData = sd.getRenderOutput();
+
+	console.log(shallowMetaData.props.children[0].props.children);
+
+	t.deepEqual(shallowMetaData.props.children[0], (<h3>Information on topic "{'Test Label'}"</h3>));
+
+	let metaList = shallowMetaData.props.children[1].props.children;
+
+	t.equal(metaList[0].props.children.join(''), ('Total mentions: 6'));
 
   t.end();
 });
